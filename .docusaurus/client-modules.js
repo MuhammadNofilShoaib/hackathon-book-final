df785fe41b95c44eec1aa8cf7242eb11dab275c5
Muhammad Nofil Shoaib <nofilshoaib@gmail.com>
2025-12-27 12:49:34 +0500

    chore: add Node/Docusaurus files to .gitignore and remove from repo

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("D:\\physical-ai-hackathon\\spec-kit-plus\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("D:\\physical-ai-hackathon\\spec-kit-plus\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("D:\\physical-ai-hackathon\\spec-kit-plus\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("D:\\physical-ai-hackathon\\spec-kit-plus\\src\\css\\custom.css"),
-];
